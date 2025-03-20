055600f4892a62a1fbcd0d40f96f4fe8b95e58b3
marianafeitosa <marianaarashiro09@gmail.com>
2025-03-19 22:05:30 -0300

    exemplo fila usando nó

---
+++ ExemploFilaNo/src/main/java/com/mycompany/exemplofilano/Main.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exemplofilano;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class Main {
+     public static void main(String args []) throws Exception{
+        //criando uma fila
+        Fila fila = new Fila();
+         fila.enfileira("A");
+         fila.enfileira("B");
+         fila.enfileira("C");
+         fila.enfileira("D");
+           
+
+        // Exibir fila
+        System.out.println("Fila:");
+       fila.imprime();
+
+        // Removendo um elemento da fila
+      fila.desenfileira();
+        
+        // Exibir a fila novamente após remoção
+        System.out.println("\nFila após remoção:");
+        fila.imprime();
+    }
+    
+    
+}
---
+++ ExemploFilaNo/src/main/java/com/mycompany/exemplofilano/No.java
@@ -0,0 +1,23 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exemplofilano;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class No {
+   private Object item;
+   No prox;
+   
+   
+  
+      public No(Object i) { 
+        item = i;
+        prox = null; 
+    }
+
+
+}
